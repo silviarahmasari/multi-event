e798cfa068f72341b0623575d5aedf3eb2fd0bc4
shaninaa <shaninaprasesti@gmail.com>
2023-06-09 22:16:42 +0700

    Update view detail verifikasi pendaftaran

--- resources/views/admin/detailpendaftaran.blade.php
+++ resources/views/admin/detailpendaftaran.blade.php
@@ -94,52 +94,127 @@
                 </div>
             </div>
         </div>
-        {{-- @foreach ($participants as $participant) --}}
-            <div class="col-12 col-sm-12 col-lg-12 px-0">
+
+        <div class="col-12 col-sm-12 col-lg-12 px-0">
                 <div class="card">
                     <div class="card-header">
                         <h4>PESERTA GROUP</h4>
+                    </div>
+                </div>
+        </div>
+        @php
+            $index = 0;
+        @endphp
+        @foreach ($participants as $participant)
+            <div class="col-12 col-sm-12 col-lg-12 px-0">
+                <div class="card">
+                    <div class="card-header">
+                        <h4>{{ $participant->participant_name }}</h4>
                         <div class="card-header-action">
-                            <a data-collapse="#mycard-collapse-lala" class="btn btn-icon btn-info"
+                            <a data-collapse="#mycard-collapse-{{ $participant->no_ktp }}" class="btn btn-icon btn-info"
                                 href="#"><i class="fas fa-plus"></i></a>
                         </div>
                     </div>
-                    <div class="collapse hide" id="mycard-collapse-lala">
+                    <div class="collapse hide" id="mycard-collapse-{{ $participant->no_ktp }}">
                         <div class="card-body">
-                            <div class="table-responsive">
-                                <table class="table table-striped" id="table-1">
-                                    <thead>
-                                        <tr>
-                                            <th class="text-center">No</th>
-                                            <th class="text-center">Nama Participant</th>
-                                            <th class="text-center">Tanggal Lahir</th>
-                                            <th class="text-center">Jenis Kelamin</th>
-                                            <th class="text-center">Alamat</th>
-                                            <th class="text-center">Tanggal Daftar</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        @foreach ($participants as $item)
-                                            <tr>
-                                                <td class="text-center">{{ $loop->iteration }}</td>
-                                                <td class="text-center">{{ $item->participant_name }}</td>
-                                                <td class="text-center">{{ $item->participant_dob }}</td>
-                                                <td class="text-center">{{ $item->participant_gender }}</td>
-                                                <td class="text-center">{{ $item->participant_address }}</td>
-                                                <td class="text-center">{{ $item->created_at }}</td>
-                                                {{-- <td class="text-center">
-                                                    <a href="{{ URL::to('detail-pendaftaran/' . $item->id_map_district_sport) }}"
-                                                        class="btn btn-warning">Detail</a>
-                                                </td> --}}
-                                            </tr>
-                                        @endforeach
-                                    </tbody>
-                                </table>
-                            </div>
+                            <form action=# method="POST"
+                                enctype="multipart/form-data">
+                                @csrf
+                                <div class="form-row">
+                                    <div class="form-group col-md-12">
+                                        <div class="form-group col-md-8">
+                                            <div class="row">
+                                                @if ($participant->pas_foto)
+                                                    <div class="col-md-3">
+                                                        <img src="{{ asset('storage/Pas_Foto/' . $participant->pas_foto) }}"
+                                                            alt="" title="" height="200px">
+                                                    </div>
+                                                    <div class="col-md-2 justify-content-center">
+                                                    </div>
+                                                @endif
+                                                <div class="col-md-3" id="preview{{$index}}"></div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="form-row">
+                                    <div class="form-group col-md-12">
+                                        <label for="participant_name">Nama Peserta</label>
+                                        <input id="participant_name" name="participant_name" placeholder="Nama Group"
+                                            class="form-control" value="{{ $participant->participant_name }}"
+                                            required="required" type="text" disabled>
+                                    </div>
+                                </div>
+                                <div class="form-row">
+                                    <div class="form-group col-md-6">
+                                        <label for="participant_gender">Jenis Kelamin</label>
+                                        <input id="participant_gender" name="participant_gender" placeholder="Nama Group"
+                                            class="form-control" value="{{ $participant->participant_gender }}"
+                                            required="required" type="text" disabled>
+                                    </div>
+                                    <div class="form-group col-md-6">
+                                        <label for="participant_dob">Tanggal Lahir</label>
+                                        <input id="participant_dob" name="participant_dob" placeholder="Nama Group"
+                                            class="form-control" value="{{ $participant->participant_dob }}"
+                                            required="required" type="date" disabled>
+                                    </div>
+                                </div>
+                                <div class="form-row">
+                                    <div class="form-group col-md-6">
+                                        <label for="participant_address">Alamat KTP</label>
+                                        <input id="participant_address" name="participant_address"
+                                            placeholder="Nama Group" class="form-control"
+                                            value="{{ $participant->participant_address }}" required="required"
+                                            type="text" disabled>
+                                    </div>
+                                    <div class="form-group col-md-6">
+                                        <label for="participant_domicile">Alamat Domisili</label>
+                                        <input id="participant_domicile" name="participant_domicile"
+                                            placeholder="Nama Group" class="form-control"
+                                            value="{{ $participant->participant_domicile }}" required="required"
+                                            type="text" disabled> 
+                                    </div>
+                                </div>
+                                <div class="form-row">
+                                    <div class="form-group col-md-6">
+                                        <label for="no_ktp">No KTP</label>
+                                        <input id="no_ktp" name="no_ktp" placeholder="Nama Group"
+                                            class="form-control" value="{{ $participant->no_ktp }}" required="required"
+                                            type="text" disabled>
+                                    </div>
+                                    <div class="form-group col-md-6">
+                                        <label for="no_kk">No Kartu Keluarga</label>
+                                        <input id="no_kk" name="no_kk" placeholder="Nama Group"
+                                            class="form-control" value="{{ $participant->no_kk }}" required="required"
+                                            type="text" disabled>
+                                    </div>
+                                </div>
+                                <div class="form-row">
+                                    <div class="form-group col-md-6">
+                                        <label for="no_akte">No Akte</label>
+                                        <input id="no_akte" name="no_akte" placeholder="Nama Group"
+                                            class="form-control" value="{{ $participant->no_akte }}" required="required"
+                                            type="text" disabled>
+                                    </div>
+                                    <div class="form-group col-md-6">
+                                        <label for="no_ijazah">No Ijazah</label>
+                                        <input id="no_ijazah" name="no_ijazah" placeholder="Nama Group"
+                                            class="form-control" value="{{ $participant->no_ijazah }}"
+                                            required="required" type="text" disabled>
+                                    </div>
+                                </div>
+                                <div class="form-group row col-auto float-right">
+                                    <button class="btn btn-success" type="submit"><i class="fa fa-plus-square"> Simpan
+                                            Perubahan </i></button>
+                                </div>
+                            </form>
                         </div>
                     </div>
                 </div>
             </div>
-        {{-- @endforeach --}}
+            @php
+                $index++;
+            @endphp
+        @endforeach
     </div>
 @endsection
